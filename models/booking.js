2b84131c0279eb4022fa9cc0df33739c3d091852
Darren <darrenzhang200027@gmail.com>
2022-01-08 14:52:32 -0500

    added cancelBooking mutation

---
+++ models/booking.js
@@ -0,0 +1,18 @@
+const { ModulesOption } = require('@babel/preset-env/lib/options')
+const mongoose = require('mongoose')
+
+const bookingSchema = new mongoose.Schema(
+    {
+        event: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Event'
+        },
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+    },
+    { timestamps: true }
+)
+
+module.exports = mongoose.model('Booking', bookingSchema)
